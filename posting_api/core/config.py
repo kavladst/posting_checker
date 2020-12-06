b3c74329f6392fcd2e2e937b8923aae12bfee7ca
vlad <vladan1020@mail.ru>
2020-12-06 19:58:05 +0300

    Update config.py

--- posting_api/core/config.py
+++ posting_api/core/config.py
@@ -2,8 +2,8 @@ from decouple import config
 
 DATABASE_HOST = config('DATABASE_HOST', default='localhost')
 DATABASE_PORT = config('DATABASE_PORT', default='5432')
-DATABASE_USER = config('DATABASE_USER', default='vlad')
-DATABASE_PASSWORD = config('DATABASE_PASSWORD', default='1234')
+DATABASE_USER = config('DATABASE_USER', default='postgres')
+DATABASE_PASSWORD = config('DATABASE_PASSWORD', default='postgres')
 DATABASE_NAME = config('DATABASE_NAME', default='posts')
 DATABASE_SCHEMA = config('DATABASE_SCHEMA', default='posts')
 
