b5df32a59cd2adcbe9a9e39eb0ab180ff2c19020
vlad <vladan1020@mail.ru>
2020-12-06 23:45:13 +0300

    Fix typo

--- scheduler/core/config.py
+++ scheduler/core/config.py
@@ -3,6 +3,8 @@ from decouple import config
 API_URL = config('API_URL', default='http://0.0.0.0:8000/')
 API_TIMEOUT_SECONDS = config('API_TIMEOUT_SECONDS', default=5, cast=int)
 
-INTERVAL_UPDATE_MINUTES = config('INTERVAL_MINUTES', default=60, cast=int)
+INTERVAL_UPDATE_MINUTES = config('INTERVAL_UPDATE_MINUTES',
+                                 default=60, cast=int)
 
-INTERVAL_SCHEDULE_MINUTES = config('INTERVAL_SCHEDULE', default=5, cast=int)
+INTERVAL_SCHEDULE_MINUTES = config('INTERVAL_SCHEDULE_MINUTES',
+                                   default=5, cast=int)
